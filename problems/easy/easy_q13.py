47221a5eb8421d0f51a75695dad090e94d5cd1ba
Sharmily-Veeramani <sharmily41@gmail.com>
2024-12-08 08:28:46 +0530

    changed the return statement

--- problems/easy/easy_q13.py
+++ problems/easy/easy_q13.py
@@ -14,8 +14,8 @@ def month_name(month):
         11: "November",
         12: "December"
     }
-    return switch[month + 1]   
+    return switch.get(month,"Invalid Month")
 if __name__ == "__main__":
-    chooseMonthNum = int(input("Enter the Month: "))
+    chooseMonthNum = int(input("Enter the Month btwn 1-12 : "))
     result = month_name(chooseMonthNum)
     print(result)
