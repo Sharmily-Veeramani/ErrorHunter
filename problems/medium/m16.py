cacda149eeab2f0ae2ce1e89132715d830e89033
Sharmily-Veeramani <sharmily41@gmail.com>
2024-12-08 12:14:38 +0530

    included punctuations

--- problems/medium/m16.py
+++ problems/medium/m16.py
@@ -3,8 +3,8 @@ import random
 import string
 
 def generate_password(length):
-    characters = string.ascii_letters + string.digits
+    characters = string.ascii_letters + string.digits+ string.punctuation
     return "".join(random.choice(characters) for i in range(length))
 
 length = int(input("Enter password length: "))
-print(generate_password(length))
+print("Generated Password : ", generate_password(length))
