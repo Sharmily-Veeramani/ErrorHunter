0e0336e6067cf33f54379e422fdef939c242cbb3
Sharmily-Veeramani <sharmily41@gmail.com>
2024-12-08 08:22:48 +0530

    if conditions were changed and in line 19 altered the word within the parenthesis

--- problems/easy/easy_q11.py
+++ problems/easy/easy_q11.py
@@ -14,26 +14,13 @@ def day_of_week(day):
         return "Invalid day"
     return switch[day]
 if __name__ == "__main__":
-    xcd = day_of_week(32)
-    print(xcd)
+    day=int(input("Enter The Number (1-7): "))
+    if 1<=day<=7:
+        xcd = day_of_week(day)
+        print(xcd)
+    else:
+        print("Enter a valid input (1-7):")
 
     
- 
-    return switch[day]   
-if __name__ == "__main__":
- 
-    day=int(input("Enter the number : "))
-    if 0<day<8:
-        xcd = day_of_week(day)
-        print(xcd)
-    else:
-        print("Enter a valid input :")
- 
-    day=int(input("Enter The Number:"))
-    if str(day) in "1234567":
-        xcd = day_of_week(day)
-        print(xcd)
-    else:
-        print("Enter Valid Input")
  
     
