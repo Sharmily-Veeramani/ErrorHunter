85e1772f0adac1fd007fd1c5d7985272ed01ce65
Sharmily-Veeramani <sharmily41@gmail.com>
2024-12-08 11:29:14 +0530

    little changes in the input function in line 8 and to enhance readability included if elif else conditions in line 20 to 25

--- problems/medium/m9.py
+++ problems/medium/m9.py
@@ -5,7 +5,7 @@ def set_operations_menu():
         print("3. Difference of Two Sets")
         print("4. Check Subset")
         print("5. Exit")
-        choice = int(input("Enter your choice: "))
+        choice = int(input("Enter your choice between 1 to 5 : "))
 
         set1 = set(input("Enter elements of Set 1 separated by space: ").split())
         set2 = set(input("Enter elements of Set 2 separated by space: ").split())
@@ -17,10 +17,12 @@ def set_operations_menu():
         elif choice == 3:
             print("Difference:", set1 - set2)   
         elif choice == 4:
-            if set1 >= set2:  
+            if set1.issubset(set2):
                 print("Set 1 is a subset of Set 2")
+            elif set2.issubset(set1):
+                print("Set 2 is a subset of Set 1")
             else:
-                print("Set 1 is not a subset of Set 2")
+                print("Neither set is a subset of the other")
         elif choice == 5:
             print("Exiting...")
             break
