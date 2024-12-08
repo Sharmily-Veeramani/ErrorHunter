e227c716a1a4f635ea6ae03badf76c78b9dd821c
Sharmily-Veeramani <sharmily41@gmail.com>
2024-12-08 12:05:05 +0530

    cnvrt for to while lp and changes made in line 6 and line 7

--- problems/medium/m14.py
+++ problems/medium/m14.py
@@ -2,9 +2,9 @@
 '''
 def fibonacci(n):
     fib_seq = [0, 1]
-    for i in range(2, n):
-        fib_seq.append(fib_seq[i-1] + fib_seq[i-2])
-    return fib_seq
+    while len(fib_seq) < n:
+        fib_seq.append(fib_seq[-1] + fib_seq[-2])
+    return fib_seq[:n]
 
 n = int(input("Enter the number of terms: "))
 print(fibonacci(n))
