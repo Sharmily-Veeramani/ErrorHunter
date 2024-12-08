82e7b015747c4badbe5f0c45de1bd58c1d18e404
Sharmily-Veeramani <sharmily41@gmail.com>
2024-12-08 08:44:25 +0530

    included input function and include a for loop to split the numbers

--- problems/easy/easy_q16.py
+++ problems/easy/easy_q16.py
@@ -1,7 +1,9 @@
 # Print the Sum of First and Last Array Element
 def sum_first_last(arr):
+    if len(arr)<1:
+        return "Array is empty"
     return arr[0] + arr[-1]  
 if __name__ == "__main__":
-    # Handle the input  by Yourself
-    arr=[2,5,8,7,6]
+    input_str = input("Enter the array elements separated by space: ")
+    arr = [int(x) for x in input_str.split()]
     print(sum_first_last(arr))
