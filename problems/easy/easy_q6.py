e9732e8a8d3140017a184d3813130a5e533abed9
Sharmily-Veeramani <sharmily41@gmail.com>
2024-12-08 07:56:35 +0530

    changed n to i and include int and deleted the last print statement to avoid None in output

--- problems/easy/easy_q6.py
+++ problems/easy/easy_q6.py
@@ -3,7 +3,6 @@ def print_numbers(n):
     i = 1
     while i <= n:
         print(i)
- 
         i+= 1   
 
 if __name__ == "__main__":
@@ -12,21 +11,4 @@ if __name__ == "__main__":
     
  
  
-        i+= 1   
-if __name__ == "__main__":
-    num = int(input("Enter the Number: "))
-    res = print_numbers(num)
-    print(res)
- 
-        i += 1   
-
-if __name__ == "__main__":
-     num = int(input("Enter the Number "))
-    print_numbers(num)
-    
- 
-    num = int(input("Enter the Number :"))
-    print_numbers(num)
- 
- 
- 
+      
