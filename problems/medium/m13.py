0e6ea6c6ede7e28bde5082051b3e61af4ed1f1b7
Sharmily-Veeramani <sharmily41@gmail.com>
2024-12-08 12:01:35 +0530

    changed the code according to the question only check whether the gn num is prime or not

--- problems/medium/m13.py
+++ problems/medium/m13.py
@@ -4,12 +4,13 @@ Create a program that checks whether a given number is a prime number or not.
 def is_prime(num):
     if num < 2:
         return False
-    for i in range(2, num):
+    for i in range(2, int(num ** 0.5) + 1):
         if num % i == 0:
             return False
     return True
 
 n = int(input("Enter a number: "))
-for i in range(2, n + 1):
-    if is_prime(i):
-        print(i)
+if is_prime(num):
+    print(f"{num} is a prime number.")
+else:
+    print(f"{num} is not a prime number.")
